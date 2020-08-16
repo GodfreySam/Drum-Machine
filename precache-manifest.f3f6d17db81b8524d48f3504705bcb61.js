cde321bc90fe2dcc6e355dc2e5f12323f06af13a
GodfreySam <godfreysam09@gmail.com>
2020-08-16 02:35:42 +0100

    Updates

---
+++ precache-manifest.f3f6d17db81b8524d48f3504705bcb61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0d9bd5ac13f61dfc7724921067c859",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "b8fe5ffdcabe3ca225c0",
+    "url": "/Drum-Machine/static/css/main.5ed2f5ec.chunk.css"
+  },
+  {
+    "revision": "d6f0319ae71494d37fdb",
+    "url": "/Drum-Machine/static/js/2.3c1b4878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.3c1b4878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fe5ffdcabe3ca225c0",
+    "url": "/Drum-Machine/static/js/main.e3bde48e.chunk.js"
+  },
+  {
+    "revision": "c6dd1430e2afb8c8d750",
+    "url": "/Drum-Machine/static/js/runtime-main.d322cb89.js"
+  }
+]);
